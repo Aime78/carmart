9f33104a87c9be7ee7ff909713aa4c8f572b077a
Rukundo Aime <rukundoaime78@gmail.com>
2024-04-08 12:19:45 +0200

    add vercel config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link';
+import { Button } from './shared/components/Button';
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
       <Link href="/about">About</Link>
+      <Button  >Hi</Button>
     </div>
   );
 }
