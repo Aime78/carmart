bf5583f65d80630bff3857555128a0dc3dcf1961
Rukundo Aime <rukundoaime78@gmail.com>
2024-04-08 12:23:09 +0200

    fix test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Link from 'next/link';
-import { Button } from './shared/components/Button';
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
       <Link href="/about">About</Link>
-      <Button  >Hi</Button>
     </div>
   );
 }
